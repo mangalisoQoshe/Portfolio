13ec8e831b644cfe2d50ab15a106842e34296a20
Mangaliso Qoshe <michael.qoshe98@gmail.com>
2023-04-19 15:22:44 +0200

    fix light-dark mode button, add blog page, and solve color conflicts

--- javascript/mySite.js
+++ javascript/mySite.js
@@ -2,6 +2,10 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navList = document.querySelector(".nav-list");
 const modeButton = document.querySelector(".mode-button");
+const sunIcon = document.querySelector(".sun-icon");
+const moonIcon = document.querySelector(".moon-icon");
+const Footer = document.querySelector(".footer");
+const primaryHeader = document.querySelector(".primary-header");
 const root = document.documentElement;
 
 var scroll = new SmoothScroll('.nav a[href*="#"]', {
@@ -23,9 +27,19 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 );
 
 modeButton.addEventListener("click", () => {
+  
   if (root.getAttribute('color-scheme') === 'dark') {
     root.setAttribute('color-scheme', 'light');
+    sunIcon.classList.remove("visually-exposed")
+    moonIcon.classList.remove("visually-hidden")
+    Footer.classList.add("active")
+    primaryHeader.classList.add("active")
   } else {
     root.setAttribute('color-scheme', 'dark');
+    sunIcon.classList.add("visually-exposed")
+    moonIcon.classList.add("visually-hidden")
+    moonIcon.classList.remove("visually-exposed")
+    Footer.classList.remove("active")
+    primaryHeader.classList.remove("active")
   }
 });
